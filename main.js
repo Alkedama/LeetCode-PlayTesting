6808db7f72b534120a47f27c666206e67388a5d8
King-Windows <krsazon@gmail.com>
2023-08-12 02:42:15 -0700

    TwoSums(#1)

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello VanillaJS!</h1>
+    <h1>Hello this is my LeetCode Play Testing!</h1>
+    <p>You can use node server-side debugging tools to check my progress in the LeetCode problems folder.</p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
---
+++ problems/TwoSums.js
@@ -0,0 +1,29 @@
+
+var twoSum = function (nums, target) {
+    const result = [];
+    const outputObj = [0, 0];
+    
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = i; j < nums.length; j++) {
+            const sum = nums[i] + nums[j];
+            result.push(sum);
+
+            if(target === sum) {
+                if(outputObj[1] < 1) {
+                    outputObj[1] = outputObj[1] + 1;
+                } else {
+                    outputObj[0] = outputObj[0] + 1;
+                    outputObj[1] = outputObj[1] + 1;
+                }
+            }
+        }
+
+    }
+return result;
+};
+
+
+console.log(twoSum([2, 7, 11, 15], 9)) // 6 loops
+// console.log(twoSum([3, 2, 4], 6)) // 3 loops
+// console.log(twoSum([3, 3], 6)) // 1 loop
