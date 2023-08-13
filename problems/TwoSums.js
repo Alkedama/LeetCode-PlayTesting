e1de7a8eb3fdae8d211dc94bf3b2f5069dd5e710
King-Windows <krsazon@gmail.com>
2023-08-13 01:38:06 -0700

    TwoSums(#5) Improves Complexity

--- problems/TwoSums.js
+++ problems/TwoSums.js
@@ -1,24 +1,14 @@
 var twoSum = function (nums, target) {
-    const outputIndex = [0, 0];
-    
+    const numIndicesMap = new Map();
     for (let i = 0; i < nums.length; i++) {
-
-        for (let j = i + 1; j < nums.length; j++) {
-        
-            const sum = nums[i] + nums[j];
-        
-            if(nums[i] + nums[j] === target) {
-                outputIndex[0] = i;
-                outputIndex[1] = j;
-            }
-
+        const complement = target - nums[i];
+        if (numIndicesMap.has(complement)) {
+            return [numIndicesMap.get(complement), i];
         }
-
+        numIndicesMap.set(nums[i], i);
     }
-return outputIndex;
 };
 
-
-console.log(twoSum([2, 7, 11, 15], 9)) // 6 loops
+console.log(twoSum([2, 7, 11, 15, 15], 22))
 console.log(twoSum([3, 2, 4], 6)) // 3 loops
 console.log(twoSum([3, 3], 6)) // 1 loop
