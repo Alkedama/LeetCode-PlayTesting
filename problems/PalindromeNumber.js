d0c83f16685391b97f15bed92d261358b81a80d5
King-Windows <krsazon@gmail.com>
2023-08-16 20:28:45 -0700

    Palindrome Number(#1)

--- problems/PalindromeNumber.js
+++ problems/PalindromeNumber.js
@@ -1,12 +1,19 @@
-const isPalindrome = function(x) {
-    new Map([
-        ['a', 1],
-        ['b', 2],
-        ['c', 3],
-    ]).forEach();
-
-    
-    return x
-};
-
-console.log(isPalindrome(132));
+function isPalindrome(number) {
+    const originalNumber = number;
+    let reversedNumber = 0;
+  
+    while (number > 0) {
+      const digit = number % 10;
+      reversedNumber = reversedNumber * 10 + digit;
+      number = Math.floor(number / 10);
+    }
+  
+    return originalNumber === reversedNumber;
+  }
+  
+  const num = 1221;
+  if (isPalindrome(num)) {
+    console.log(`${num} is a palindrome number.`);
+  } else {
+    console.log(`${num} is not a palindrome number.`);
+  }
